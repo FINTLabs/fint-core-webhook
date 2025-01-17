ce5c13ef6a35612521ec2ae339a8f56286f65d22
Hknots <hknots99@gmail.com>
2025-01-17 01:53:30 +0100

    create WebHookServer

---
+++ src/main/kotlin/no/fintlabs/webhook/server/annotation/WebHookServer.kt
@@ -0,0 +1,10 @@
+package no.fintlabs.webhook.server.annotation
+
+/**
+ * By annotating a class with @WebhookServer, we import the required
+ * configuration to set up the serer side of fint-core-webhook.
+ */
+@MustBeDocumented
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class WebHookServer()
