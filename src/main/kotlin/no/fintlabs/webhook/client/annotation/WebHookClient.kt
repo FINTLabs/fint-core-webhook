ec12ab013fa1df9ca2eb0448346032e4ad40b882
Hknots <hknots99@gmail.com>
2025-01-17 01:53:39 +0100

    create WebHookClient annotation

---
+++ src/main/kotlin/no/fintlabs/webhook/client/annotation/WebHookClient.kt
@@ -0,0 +1,10 @@
+package no.fintlabs.webhook.client.annotation
+
+/**
+ * By annotating a class with @WebhookClient, we import the required
+ * configuration to set up the client side of fint-core-webhook.
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class WebHookClient()
