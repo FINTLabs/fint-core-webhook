25bc57588fd41adb3cde09a3170399ab90dce5b4
Hknots <hknots99@gmail.com>
2025-01-17 01:55:12 +0100

    create WebHookServerService class

---
+++ src/main/kotlin/no/fintlabs/webhook/server/WebHookServerService.kt
@@ -0,0 +1,37 @@
+package no.fintlabs.webhook.server
+
+import no.fintlabs.webhook.server.annotation.WebHookServer
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+
+@Service
+@ConditionalOnBean(annotation = [WebHookServer::class])
+class WebHookServerService(
+    private val webHookCache: WebHookCache,
+    private val webClient: WebClient = WebClient.create()
+) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun callback(any: Any) {
+        webHookCache.callbacks.forEach { callback ->
+            webClient.post()
+                .uri("$callback/event")
+                .bodyValue(any)
+                .retrieve()
+                .toBodilessEntity()
+                .subscribe(
+                    {
+                        logger.debug("Successuly sent payload to: $callback")
+                    },
+                    {
+                        logger.warn("Failed to send payload, detaching callback: $callback")
+                        webHookCache.removeCallback(callback)
+                    }
+                )
+        }
+    }
+
+}
