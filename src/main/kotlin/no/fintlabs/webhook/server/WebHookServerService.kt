516ce3ae8181ec9b3ccdcd0122e0535440e88e31
Hknots <hknots99@gmail.com>
2025-01-22 15:05:46 +0100

    move creation of webclient outside of constructor

--- src/main/kotlin/no/fintlabs/webhook/server/WebHookServerService.kt
+++ src/main/kotlin/no/fintlabs/webhook/server/WebHookServerService.kt
@@ -8,12 +8,10 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 @ConditionalOnBean(annotation = [WebHookServer::class])
-class WebHookServerService(
-    private val webHookCache: WebHookCache,
-    private val webClient: WebClient = WebClient.create()
-) {
+class WebHookServerService(private val webHookCache: WebHookCache) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private val webClient: WebClient = WebClient.create()
 
     fun callback(any: Any) {
         webHookCache.callbacks.forEach { callback ->
