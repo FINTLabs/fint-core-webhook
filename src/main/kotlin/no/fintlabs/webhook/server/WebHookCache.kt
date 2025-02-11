1d4b5f84e21ea5ba18b242a505a8242dd1ae99ba
Hknots <hknots99@gmail.com>
2025-02-11 08:35:21 +0100

    feat: add types to callback cache

--- src/main/kotlin/no/fintlabs/webhook/server/WebHookCache.kt
+++ src/main/kotlin/no/fintlabs/webhook/server/WebHookCache.kt
@@ -3,17 +3,23 @@ package no.fintlabs.webhook.server
 import no.fintlabs.webhook.server.annotation.WebHookServer
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
 
 @Component
 @ConditionalOnBean(annotation = [WebHookServer::class])
 class WebHookCache {
 
-    val callbacks: MutableSet<String> = mutableSetOf()
+    val callbacks: ConcurrentHashMap<String, MutableSet<String>> = ConcurrentHashMap()
 
-    fun addCallback(callback: String) = callbacks.add(callback)
+    fun addCallback(clazz: Class<*>, callback: String) =
+        callbacks.computeIfAbsent(clazz.name) { _: String -> mutableSetOf() }
+            .add(callback)
 
-    fun callbackExists(callback: String) = callbacks.contains(callback)
+    fun getCallbacks(clazz: Class<*>) =
+        callbacks.getOrDefault(clazz.name, mutableSetOf())
 
-    fun removeCallback(callback: String) = callbacks.remove(callback)
+    fun callbackExists(clazz: Class<*>, callback: String) =
+        callbacks.getOrDefault(clazz.name, mutableSetOf())
+            .contains(callback)
 
 }
