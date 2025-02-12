a2ec32626fd7fd44d848f089cc3235081bf2c341
Hknots <hknots99@gmail.com>
2025-02-12 15:39:30 +0100

    refactor: rename bean

--- src/main/kotlin/no/fintlabs/webhook/client/config/WebClientConfig.kt
+++ src/main/kotlin/no/fintlabs/webhook/client/config/WebClientConfig.kt
@@ -1,16 +1,16 @@
 package no.fintlabs.webhook.client.config
 
-import no.fintlabs.webhook.client.annotation.WebHookClient
+import no.fintlabs.webhook.client.annotation.WebhookClient
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-@ConditionalOnBean(annotation = [WebHookClient::class])
+@ConditionalOnBean(annotation = [WebhookClient::class])
 class WebClientConfig {
 
-    @Bean
-    fun webHookClient() = WebClient.create()
+    @Bean("webhookClient")
+    fun webhookClient() = WebClient.create()
 
 }
