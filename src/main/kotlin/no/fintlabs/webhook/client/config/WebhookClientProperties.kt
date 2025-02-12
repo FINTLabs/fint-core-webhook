e37f15ee7c29ca09fa235080d1c866a4d92addb8
Hknots <hknots99@gmail.com>
2025-02-12 15:40:49 +0100

    refactor/feat: setup callbacks to be a Map<String, MutableList<String>>

--- src/main/kotlin/no/fintlabs/webhook/client/config/WebhookClientProperties.kt
+++ src/main/kotlin/no/fintlabs/webhook/client/config/WebhookClientProperties.kt
@@ -1,14 +1,14 @@
 package no.fintlabs.webhook.client.config
 
-import no.fintlabs.webhook.client.annotation.WebHookClient
+import no.fintlabs.webhook.client.annotation.WebhookClient
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationProperties(prefix = "fint.webhook.client")
-@ConditionalOnBean(annotation = [WebHookClient::class])
+@ConditionalOnBean(annotation = [WebhookClient::class])
 data class WebhookClientProperties(
     var server: String = "",
-    var callback: String = ""
+    var callbacks: Map<String, MutableList<String>> = mutableMapOf()
 )
