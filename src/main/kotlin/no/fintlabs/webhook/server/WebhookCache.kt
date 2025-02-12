be8e4b0bb9f80f75debb5a863c7ab2e6bc55353f
Hknots <hknots99@gmail.com>
2025-02-12 15:39:56 +0100

    feat: allow removing of callback & support more events

---
+++ src/main/kotlin/no/fintlabs/webhook/server/WebhookCache.kt
@@ -0,0 +1,29 @@
+package no.fintlabs.webhook.server
+
+import no.fintlabs.webhook.server.annotation.WebhookServer
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+@ConditionalOnBean(annotation = [WebhookServer::class])
+class WebhookCache {
+
+    val cache: ConcurrentHashMap<String, MutableSet<String>> = ConcurrentHashMap()
+
+    fun addCallbacks(callbacks: Map<String, Collection<String>>) =
+        callbacks.forEach {
+            cache.computeIfAbsent(it.key) { mutableSetOf() }
+                .addAll(it.value)
+        }
+
+    fun getCallbacks(type: String) =
+        cache.getOrDefault(type, mutableSetOf())
+
+    fun callbacksExists(callbacks: Map<String, Collection<String>>) =
+        callbacks.all { cache.contains(it.key) && cache.getOrDefault(it.key, mutableSetOf()).containsAll(it.value) }
+
+    fun removeCallback(eventName: String, callback: String) =
+        cache.getOrDefault(eventName, mutableSetOf()).remove(callback)
+
+}
