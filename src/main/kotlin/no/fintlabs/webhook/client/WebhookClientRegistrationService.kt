7f8193d7c9b6aa459082d4441a46facc34d1208d
Hknots <hknots99@gmail.com>
2025-02-13 13:18:34 +0100

    fix!: remove circular dependency & set correct bodyValue

--- src/main/kotlin/no/fintlabs/webhook/client/WebhookClientRegistrationService.kt
+++ src/main/kotlin/no/fintlabs/webhook/client/WebhookClientRegistrationService.kt
@@ -14,28 +14,27 @@ import reactor.core.publisher.Mono
 class WebhookClientRegistrationService(
     properties: WebhookClientProperties,
     @Qualifier("webhookClient")
-    private val webhookClient: WebClient,
-    private val handlerRegistry: WebhookHandlerRegistry
+    private val webhookClient: WebClient
 ) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val registerUrl = "${properties.server}/webhook/register"
     private val unregisterUrl = "${properties.server}/webhook/unregister"
 
-    fun unregister(clazzName: String, callbacks: Collection<String>, url: String = unregisterUrl) =
+    fun unregister(eventName: String, callbacks: Collection<String>, url: String = unregisterUrl) =
         webhookClient.post()
             .uri(url)
-            .bodyValue(handlerRegistry.getCallbacks())
+            .bodyValue(mapOf(eventName to callbacks))
             .exchangeToMono { Mono.just(it.statusCode()) }
             .subscribe(
                 { logger.info("Webhook unregistered successfully!") },
                 { error -> logger.warn("Webhook unregistering failed: Server is likely down. Error: ${error.message}") }
             )
 
-    fun register(clazzName: String, callbacks: Collection<String>, url: String = registerUrl) =
+    fun register(eventName: String, callbacks: Collection<String>, url: String = registerUrl) =
         webhookClient.post()
             .uri(unregisterUrl)
-            .bodyValue(handlerRegistry.getCallbacks())
+            .bodyValue(mapOf(eventName to callbacks))
             .exchangeToMono { Mono.just(it.statusCode()) }
             .subscribe(
                 { logger.info("Webhook registered successfully!") },
